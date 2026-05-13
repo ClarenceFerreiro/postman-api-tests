c6c4de0254711b51126358b7d49cef90722e99be
ClarenceFerreiro <7901597@gmail.com>
2026-05-14 01:12:00 +0300

    Fix Jest config: remove allure reporter

--- api-tests-ts/jest.config.js
+++ api-tests-ts/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  reporters: ['default', 'jest-allure']
+  testMatch: ['**/*.test.js', '**/*.spec.js'],
+  collectCoverageFrom: ['**/*.js'],
+  coverageDirectory: 'coverage'
 };
