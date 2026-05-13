0de7e66ec904932c3dc1254f80c361f23be868f5
ClarenceFerreiro <7901597@gmail.com>
2026-05-14 00:39:32 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/*.test.js', '**/*.spec.js'],
+  collectCoverageFrom: ['**/*.js'],
+  coverageDirectory: 'coverage',
+  reporters: ['default']
 };
